196053a7d2e5888f368f123f284da36ae8811393
shivank8 <shivank8@users.noreply.github.com>
2020-12-29 22:55:28 +0530

    Added Firebase service with database

--- app/src/main/java/com/adreno/bookstore/activity/RegisterActivity.kt
+++ app/src/main/java/com/adreno/bookstore/activity/RegisterActivity.kt
@@ -1,36 +1,33 @@
 package com.adreno.bookstore.activity
 
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.adreno.bookstore.R
-import com.adreno.bookstore.util.SessionManager
-import com.adreno.bookstore.util.Validations
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
+    lateinit var auth: FirebaseAuth
+    var databaseReference: DatabaseReference? = null
+    var database: FirebaseDatabase? = null
     lateinit var toolbar: Toolbar
-    lateinit var btnRegister: Button
-    lateinit var etName: EditText
-    lateinit var etPhoneNumber: EditText
-    lateinit var etPassword: EditText
-    lateinit var etEmail: EditText
-    lateinit var etAddress: EditText
-    lateinit var etConfirmPassword: EditText
-    lateinit var rlRegister: RelativeLayout
-    lateinit var sharedPreferences: SharedPreferences
-    lateinit var sessionManager: SessionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        sharedPreferences =
-            getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database?.reference!!.child("profile")
+
+
+
 
         toolbar = findViewById(R.id.toolbarr)
         setSupportActionBar(toolbar)
@@ -39,46 +36,61 @@ class RegisterActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toolbar.setTitleTextAppearance(this, R.style.PoppinsTextAppearance)
 
-        rlRegister = findViewById(R.id.rlRegister)
-        etName = findViewById(R.id.etName)
-        etPhoneNumber = findViewById(R.id.etPhoneNumber)
-        etEmail = findViewById(R.id.etEmail)
-        etPassword = findViewById(R.id.etPassword)
-        etConfirmPassword = findViewById(R.id.etConfirmPassword)
-        etAddress = findViewById(R.id.etAddress)
-        btnRegister = findViewById(R.id.btnRegister)
+        register()
 
 
-        sessionManager = SessionManager(this)
-        sharedPreferences =
-            this.getSharedPreferences(sessionManager.PREF_NAME, sessionManager.PRIVATE_MODE)
+        }
 
-        btnRegister.setOnClickListener {
+        private fun register() {
+            btnRegister.setOnClickListener {
 
-            val name: String = etName.text.toString()
-            val phone: String = etPhoneNumber.text.toString()
-            val email: String = etEmail.text.toString()
-            val password : String = etPassword.text.toString()
-            val cnfpassword: String = etConfirmPassword.text.toString()
-            if(Validations.validateMobile(phone) and Validations.validateNameLength(name) and Validations.validateEmail(email) and
-                   Validations.validatePasswordLength(password) and Validations.validatePasswordLength(cnfpassword) and Validations.matchPassword(password,cnfpassword)) {
+                when {
+                    TextUtils.isEmpty(etName.text.toString()) -> {
+                        etName.error = "Please enter first name "
+                        return@setOnClickListener
+                    }
+                    TextUtils.isEmpty(etEmail.text.toString()) -> {
+                        etEmail.error = "Please enter last name "
+                        return@setOnClickListener
+                    }
+                    TextUtils.isEmpty(etPhoneNumber.text.toString()) -> {
+                        etPhoneNumber.error = "Please enter user name "
+                        return@setOnClickListener
+                    }
+                    TextUtils.isEmpty(etPassword.text.toString()) -> {
+                        etPassword.error = "Please enter password "
+                        return@setOnClickListener
+                    }
+                    else -> auth.createUserWithEmailAndPassword(
+                        etEmail.text.toString(),
+                        etPassword.text.toString()
+                    )
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                val currentUser = auth.currentUser
+                                val currentUSerDb = databaseReference?.child((currentUser?.uid!!))
+                                currentUSerDb?.child("firstname")
+                                    ?.setValue(etName.text.toString())
+                                currentUSerDb?.child("lastname")
+                                    ?.setValue(etPhoneNumber.text.toString())
 
-                 sessionManager.setLogin(true)
+                                Toast.makeText(
+                                    this,
+                                    "Registration Success. ",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                finish()
 
-                sharedPreferences.edit().putString("name", name)
-                    .apply()
-                sharedPreferences.edit()
-                    .putString("email", email)
-                    .apply()
-                sharedPreferences.edit()
-                    .putString("mobileNumber", phone)
-                    .apply()
-
-                val intent = Intent(this, DashboardActivity::class.java)
-                startActivity(intent)
+                            } else {
+                                Toast.makeText(
+                                    this,
+                                    "Registration failed, please try again! ",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                }
             }
-            else
-                Toast.makeText(this,"Please enter valid details",Toast.LENGTH_SHORT).show()
         }
     }
-}
+
